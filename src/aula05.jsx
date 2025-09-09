435a1708342de4e03686bda7bc6d675b9a998967
Fadinha2ds2025 <geovana.martius@escola.pr.gov.br>
2025-09-09 07:42:45 -0300

    Add files via upload

--- src/aula05.jsx
+++ src/aula05.jsx
@@ -1,54 +1,68 @@
-import React, { Component} from 'react';
-import { View, Text, StyleSheet }  from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet,Image } from 'react-native';
 
-class Aula05 extends Component {
-    render(){
-        return (
-            <View style={ styles.container }>
-                <View style={ styles.caixa1 }>
-                  <Text style={{ color: 'white'}}>1</Text>
-                </View>
-                <View style={ styles.caixa2 }>
-                  <Image />
-                </View>
-                <View style={ styles.caixa3 }>3</View>
-            </View>
-        );
-    }
+class Aula05  extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={ styles.caixa1 }>1</View>
+        <View style={ styles.caixa2 }>
+          <Image
+          style={ styles.img1 }
+          source={{ uri: 'https://webterra.com.br/wp-content/uploads/2024/03/Pernambuco-400x400.jpg'}}
+          />
+        </View>
+        <View style={ styles.caixa3 }>
+            <Image>
+                
+            </Image>
+        </View>
+      </View>
+    );
+  }
 }
 
 export default Aula05;
 
 const styles = StyleSheet.create({
-    container: {
-        flex:1,
-        backgroundColor: 'white',
-    },
+  container: {
+     flex: 1,
+     backgroundColor: 'white',
+  },
 
-    caixa1: {
-        height:100,
-        backgroundColor: 'red',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: 'white',
-        fontSize: 20
-    },
 
-    caixa2: {
-         flex: 1,
-        backgroundColor: 'green',
-         color: 'white',
-        fontSize: 20,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
+  caixa1: {
+    height: 100,
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+    color: 'white',
+    fontSize: 20
+  },
 
-    caixa3: {
-        backgroundColor: 'blue',
-        height: 100,
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: 'white',
-        fontSize: 20,
-    },
+
+  caixa2: {
+   flex: 1,
+   backgroundColor: 'green',
+   alignItems: 'center',
+   justifyContent: 'center',
+   alignItems: 'center'
+   
+   
+},
+
+  caixa3: {
+   backgroundColor: 'blue',
+   height: 100,
+   alignItems: 'center',
+   justifyContent: 'center',
+   color: 'white',
+   fontSize: 20
+},
+
+img1: {
+    width: 50,
+    height: 50,
+},
+
 })
